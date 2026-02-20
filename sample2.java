90ac1cadd957f65bc22a67da0e37f432556ef5bd
Siddhi@10 <siddhidhane2@gmail.com>
2026-02-20 11:52:21 +0530

    add

---
+++ sample2.java
@@ -0,0 +1,5 @@
+public class Main { // Defines a class named Main
+    public static void main(String[] args) { // The main method, where execution begins
+        System.out.println("Hello, World!"); // Prints the string to standard output, followed by a new line
+    }
+}
